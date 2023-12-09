188ba34a87a5f466e7a38c1aff28806211676495
lijintao <481411494@qq.com>
2023-12-10 01:44:11 +0800

    首页布局

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
-<template>
-</template>
-<script  lang="ts" setup></script>
-<style>
-	/* #ifdef APP-PLUS-NVUE */
-	@import './tmui/scss/nvue.css';
-	/* #endif */
-	/* #ifndef APP-PLUS-NVUE */
-	@import './tmui/scss/noNvue.css';
-	/* #endif */
+<template></template>
+<script lang="ts" setup>
+import { useTmpiniaStore } from "@/tmui/tool/lib/tmpinia";
+const store = useTmpiniaStore();
 
-
-
+store.setTmVuetifyAddTheme("theme-yellow", "#eee353", true);
+</script>
+<style lang="scss">
+// 阿里云字体图标
+@import "@/iconfont/index.scss";
+/* #ifdef APP-PLUS-NVUE */
+@import "./tmui/scss/nvue.css";
+/* #endif */
+/* #ifndef APP-PLUS-NVUE */
+@import "./tmui/scss/noNvue.css";
+/* #endif */
 </style>
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -43,7 +43,72 @@
         :list="['恭喜xxxx用户下单成功', '热烈庆祝xxxx中奖免单']"
         :margin="[0, 0]"
         class="notice"
-      ></tm-roll-notice>
+        followTheme
+      />
+      <view class="statistics-wrap">
+        <view class="item">
+          <view class="tit">
+            <text class="num">3.77</text>
+            <text class="util">万</text>
+          </view>
+          <view class="subtit">专业入驻师傅</view>
+        </view>
+
+        <view class="item">
+          <view class="tit">
+            <text class="num">15.13</text>
+            <text class="util">万</text>
+          </view>
+          <view class="subtit">累计服务订单</view>
+        </view>
+
+        <view class="item">
+          <view class="tit">
+            <text class="num">97.9</text>
+            <text class="util">%</text>
+          </view>
+          <view class="subtit">服务好评率</view>
+        </view>
+      </view>
+      <tm-image
+        preview
+        :width="670"
+        :height="277"
+        :src="dispatch_order"
+        class="img"
+      />
+      <view class="order-process-wrap">
+        <view class="tit">下单流程</view>
+        <view class="step-wrap">
+          <view class="step-item">
+            <view class="icon">
+              <text class="iconfont icon-icon_fabu"></text>
+            </view>
+            <view class="text">发布需求</view>
+          </view>
+          <text class="next iconfont icon-xiayibu"></text>
+          <view class="step-item">
+            <view class="icon">
+              <text class="iconfont icon-gerentouxiang_o"></text>
+            </view>
+            <view class="text">分配师傅</view>
+          </view>
+          <text class="next iconfont icon-xiayibu"></text>
+          <view class="step-item">
+            <view class="icon">
+              <text class="iconfont icon-weixiu"></text>
+            </view>
+            <view class="text">上门/到店服务</view>
+          </view>
+          <text class="next iconfont icon-xiayibu"></text>
+          <view class="step-item">
+            <view class="icon">
+              <text class="iconfont icon-fuwutiyandingyi"></text>
+            </view>
+            <view class="text">验收评价</view>
+          </view>
+        </view>
+      </view>
     </view>
   </tm-app>
 </template>
@@ -51,6 +116,7 @@
 <script lang="ts" setup>
 import { ref } from "vue";
 import steering_wheel from "@/static/steering_wheel.png";
+import dispatch_order from "@/static/dispatch_order.jpg";
 const searchValue = ref("");
 </script>
 
@@ -105,5 +171,81 @@ const searchValue = ref("");
   border-top-right-radius: 40rpx;
   background-color: #fff;
   padding: 40rpx;
+  .statistics-wrap {
+    margin: 50rpx 0;
+    display: flex;
+    justify-content: space-around;
+    .item {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      .tit {
+        margin-bottom: 10rpx;
+        font-weight: 600;
+        .num {
+          font-size: 40rpx;
+        }
+        .util {
+          margin-left: 10rpx;
+        }
+      }
+      .subtit {
+        font-size: 24rpx;
+        color: #999;
+      }
+    }
+  }
+  .order-process-wrap {
+    position: relative;
+    height: 250rpx;
+    margin-top: 40rpx;
+    background-color: $uni-color-primary;
+    border-radius: 20rpx;
+    .tit {
+      position: absolute;
+      left: 0;
+      top: 0;
+      padding: 10rpx 40rpx;
+      background-color: #000;
+      color: $uni-color-primary;
+      font-size: 24rpx;
+      border-top-left-radius: 20rpx;
+      border-bottom-right-radius: 20rpx;
+    }
+    .step-wrap {
+      position: absolute;
+      top: 50%;
+      transform: translateY(-50%);
+      width: 100%;
+      display: flex;
+      align-items: center;
+      justify-content: space-around;
+      font-size: 24rpx;
+      .step-item {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        .icon {
+          width: 60rpx;
+          height: 60rpx;
+          background-color: #e8d262;
+          border-radius: 50%;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          color: #666;
+        }
+        .text {
+          margin-top: 15rpx;
+          font-size: 24rpx;
+          transform: scale(0.8);
+        }
+      }
+      .next {
+        color: #fff;
+        font-size: 44rpx;
+      }
+    }
+  }
 }
 </style>
