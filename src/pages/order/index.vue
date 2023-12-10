b36c40b915897448ee9f1f40187ea8d4834e6bb4
lijintao <481411494@qq.com>
2023-12-10 13:58:49 +0800

    订单

--- src/pages/order/index.vue
+++ src/pages/order/index.vue
@@ -1,7 +1,35 @@
 <template>
-  <view> order </view>
+  <tm-app>
+    <tm-input
+      v-model="searchValue"
+      prefix="tmicon-search"
+      showClear
+      placeholder="搜索您想查找的订单"
+      placeholderStyle="font-size: 24rpx"
+      :height="80"
+    />
+    <tm-segtab
+      :width="750"
+      :list="tabsTitle"
+      :height="100"
+      defaultValue="1"
+    ></tm-segtab>
+  </tm-app>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { ref } from "vue";
 
-<style lang="scss" scoped></style>
+const searchValue = ref("");
+
+const tabsTitle = ref([
+  { id: "1", text: "全部订单" },
+  { id: "2", text: "退款记录" },
+  {
+    id: "3",
+    text: "申诉记录",
+  },
+]);
+</script>
+
+<style lang="scss"></style>
