7c3683aa94c7f44ff4816eddee6c14c7bd58fc26
lijintao <481411494@qq.com>
2023-12-15 10:52:26 +0800

    部分表单布局

--- src/pages/createOrder/index.vue
+++ src/pages/createOrder/index.vue
@@ -9,15 +9,20 @@
       :padding="[0, 0]"
       :margin="[20]"
       :border="false"
+      style="background-color: rgba(245, 245, 245, 1)"
     >
       <tm-sheet :padding="[0, 0]" :margin="[20]" :round="[4]">
         <tm-text :font-size="24" _class="text-weight-b" label="车主信息" />
         <tm-divider />
         <tm-form-item required label="车主姓名" field="name" :errHeight="15">
-          <tm-input v-model="formData.name"></tm-input>
+          <tm-input v-model="formData.name" placeholder="请输入车主姓名" />
         </tm-form-item>
         <tm-form-item required label="电话" field="phone" :errHeight="15">
-          <tm-input v-model="formData.phone" type="number"></tm-input>
+          <tm-input
+            v-model="formData.phone"
+            type="number"
+            placeholder="请输入电话号码"
+          />
         </tm-form-item>
         <tm-form-item required label="地址" field="address" :errHeight="15">
           <tm-input
@@ -26,6 +31,7 @@
             :readonly="true"
             :adjustPosition="false"
             :disabled="true"
+            placeholder="请选择地址"
           />
         </tm-form-item>
         <tm-form-item
@@ -38,8 +44,32 @@
             v-model="formData.addressDesc"
             type="textarea"
             :height="150"
+            placeholder="请输入详情地址"
           />
         </tm-form-item>
+      </tm-sheet>
+
+      <tm-sheet
+        :padding="[20]"
+        :margin="[0, 20]"
+        :round="[4]"
+        style="background-color: rgba(245, 245, 245, 1)"
+      >
+        <view class="add-card-wrap">
+          <tm-text :font-size="24" _class="text-weight-b" label="车辆信息" />
+          <tm-button size="small" :margin="[0]">添加车辆</tm-button>
+        </view>
+
+        <tm-divider />
+        <tm-sheet :padding="[0, 0]" :margin="[0]" :round="[4]">
+          <tm-text
+            :font-size="28"
+            label="请添加车辆"
+            class="flex-center"
+            color="#999"
+            :padding="[0, 20]"
+          />
+        </tm-sheet>
       </tm-sheet>
     </tm-form>
 
@@ -66,6 +96,7 @@ const formData = ref({
   addressDesc: "",
   proExit: true,
   desc: "",
+  car: "",
 });
 
 const confirm = () => {
@@ -77,4 +108,10 @@ watch(citydate, (nVal) => {
 });
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.add-card-wrap {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+</style>
