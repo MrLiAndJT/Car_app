95472cd3998a59b81a8dbf9660ca64923031737f
lijintao <481411494@qq.com>
2024-01-07 17:19:17 +0800

    删除

--- src/pages/order/components/OrderCard.vue
+++ src/pages/order/components/OrderCard.vue
@@ -54,7 +54,6 @@
               shape="circle"
               text="修改订单"
               :customStyle="btnCss"
-              @click="getOrderDetail(item.id)"
             />
             <up-button
               type="primary"
@@ -114,11 +113,6 @@ const getOrderList = async () => {
   const data = await Main.userOrderList();
   orderList.value = data.data || [];
   uni.hideLoading();
-};
-
-const getOrderDetail = async (id: number) => {
-  const data = await Main.userOrderGet(id);
-  console.log("详情: ", data);
 };
 
 watch(
