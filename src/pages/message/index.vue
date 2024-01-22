1cbbf14b7be9f63137c619a96d1658c407697577
lijintao <481411494@qq.com>
2024-01-22 21:04:18 +0800

    消息列表

--- src/pages/message/index.vue
+++ src/pages/message/index.vue
@@ -1,13 +1,16 @@
 <template>
-  <view>
-    消息
+  <view class="page-container">
+    <u-empty mode="message" />
   </view>
 </template>
 
-<script lang="ts" setup>
-  
-</script>
+<script lang="ts" setup></script>
 
 <style lang="scss" scoped>
-
+.page-container {
+  min-height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
 </style>
